3d57b2118011895626dbdc563a89cf591dac2820
dpodboj1 <podbojec.damir@gmail.com>
2017-12-16 21:41:28 +0100

    Updated dependencies and eslintrc

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import App from './App';
+import App from './containers/App';
 
-const renderApp = Component => {
-	render(
-		<AppContainer>
-			<Component />
-		</AppContainer>,
-		document.getElementById('root')
-	);
+const renderApp = (Component) => {
+  render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root'),
+  );
 };
 
 renderApp(App);
 
 if (module.hot) {
-	module.hot.accept();
+  module.hot.accept();
 }
